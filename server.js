b9608f180a60fbd9f86fc0f7a66877a7dfa671c1
ShyamKumar11 <Shyamkumarlohat@gmail.com>
2022-05-21 12:37:45 -0600

    Redis Hashed Password

--- server.js
+++ server.js
@@ -1,41 +1,90 @@
 const express = require("express"); // importing library 
+
+
+const port = 3000; // making a variable "Const"
+const app = express(); // use the library  (express is a function) calling express function that returns an object
+const md5 = require("md5") // importing md5 libarary 
 const bodyParser = require("body-parser"); // body parser is called middleware
-const port = 3000;
-const app = express(); // use the library  (express is a function)
-const md5 = require("md5")
 
-app.use(bodyParser.json()); // use the middleware (call it beofre anything happens on each request)
+const {createClient} = require("redis");
+const { response } = require("express");
 
-app.listen (port,()=>{
+const redisClient = createClient ( // This code will run when I start redis // THis will connect to the credentails
+
+{
+socket:{
+    port:6379,
+    host:"127.0.0.1", 
+ }, 
+
+}); // this creates a connection to the redis database
+
+
+app.use(bodyParser.json()); // use the middleware (call it beofre anything happens on each request) .jason is a function. 
+
+app.listen (port, async()=>{
+    await redisClient.connect(); // Creating TCP socket with redis. @
     console.log("listening on port: "+port)  // waiting for network request.
 })  // listening    
 
-app.get('/',(req,res)=>
-{res.send("Hello");
-})   // slash / means there are no parameters. 
+
+// app.get('/',(request,response)=>
+// {res.send("Hello");
+// hm  // slash / means there are no parameters. 
 // port 80 is resreved but we use non-standard port (443 is the secured and 80 is not)
 // if you use a port more than a 1000 you don't have to be an admisntrator. 
 
 
-app.post("/login", (request, response)=>{  // a post is wheb a client sends new information to an API (curl in appclient calling the functions)
+// const validatepassword = async (reques, response)=>{
+//     await redisclient.connect(); // createing a TCP socket with redis 
+//     const requesthasedpassword = md5(request.body.credentails);
+//     const redishashedpassword = await redisclient.hmGet("credentails",request.body.username);
+//     const loginRequest = request.body;
+//     console.log("Request Body", JSON.stringify(request.body));
+//     //search database for username and retrive current password 
+// }
+
+
+const validatepassword = async (request, response)=>{
+    const requesthasedpassword = md5(request.body.password);
+    console.log ("The request hashed password "+requesthasedpassword);
+    const redishashedpassword = await redisClient.hmGet("credentials", request.body.userName); // read password from redis
+    console.log ("The redis hashed password "+redishashedpassword);
     const  loginRequest = request.body;
+    console.log("Request Body", JSON.stringify(request.body));
     // search database username and retrive current password
 
     // Compare the hash version of the password that was sent with the hashed version from the database 
 
-    if (loginRequest.userName == "kumar_32@gmail.com" && loginRequest.password == "Stedi@3456"){  // login request is an object and each object has bubch of feilds, so user name and password are a field)
-        response.status(200); // 200 menas ok
+
+    if (requesthasedpassword==redishashedpassword){
+        response.status(200);
         response.send("Welcome");
     } else{
-        response.status(401); //401 means unauthorized
-        response.send("Unauthroized");
-    }
+        response.status(401);
+        response.send("Unauthorized");
 
-});
+    }
+    
+
+}
 
 app.get("/" , (request, response)=>{  //Every tinme something calls your API that is a request. 
     response.send("Hello"); // A response is when an API gives the required information.
 })
 
+app.post("/login", validatepassword);
 
+
+
+// Create a post where you would create a new user and password
+
+app.post ('/signup; signup');
+const singup =  (request, response)=>{
+
+
+
+
+
+}
 
